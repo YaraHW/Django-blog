f3f5a2df442bbdd84a353a94939f7ff57e2f082e
YaraHW <mister.chepurnoy@mail.ru>
2021-11-14 21:34:48 +0700

    login.html was added

--- PL/urls.py
+++ PL/urls.py
@@ -21,6 +21,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('dashboard/', views.index, name='index'),
+    path('accounts/', include('django.contrib.auth.urls')),
 
 ]
 
