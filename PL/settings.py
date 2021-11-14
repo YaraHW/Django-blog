705655e0b9236abd5114f473221dfd1acbcd1666
YaraHW <mister.chepurnoy@mail.ru>
2021-11-14 20:05:03 +0700

    requirements.txt was made

--- PL/settings.py
+++ PL/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'PL.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangoblog',
+        'USER': 'yaroslav',
+        'PASSWORD': 'yaroslav',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
